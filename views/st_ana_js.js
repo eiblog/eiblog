31d3b8c43f732971f4f0cecadf4092f91be75fbd
deepzz0 <deepzz.qi@gmail.com>
2016-10-17 21:12:22 +0800

    update static file

--- views/st_ana_js.js
+++ views/st_ana_js.js
@@ -1,3 +1,3 @@
 {{define "ana_js"}}
-! function(e, n, o, t) {var a = e.screen,r = encodeURIComponent,i = ["tid=UA-77251712-1", "dl=" + r(n.URL), "dt=" + r(n.title), "dr=" + r(n.referrer), "dp=" + r(t.pathname), "ul=" + (o.language || o.browserLanguage).toLowerCase(), "sd=" + a.colorDepth + "-bit", "sr=" + a.width + "x" + a.height, "_=" + +new Date],c = "?" + i.join("&");e.__beacon_img = new Image, e.__beacon_img.src = "/beacon.html" + c }(window, document, navigator, location);
+!function(e,n,o){var t=e.screen,a=encodeURIComponent,r=["dt="+a(n.title),"dr="+a(n.referrer),"ul="+(o.language||o.browserLanguage),"sd="+t.colorDepth+"-bit","sr="+t.width+"x"+t.height,"_="+ +new Date],i="?"+r.join("&");e.__beacon_img=new Image,e.__beacon_img.src="/beacon.html"+i}(window,document,navigator,location);
 {{end}}
