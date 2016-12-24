6cdb3e2f9be05067fc40456ee84b874753de76ec
deepzz0 <deepzz.qi@gmail.com>
2016-12-25 02:26:51 +0800

    fix sh

--- dist.sh
+++ dist.sh
@@ -8,6 +8,11 @@ for os in linux darwin windows; do
     echo "... building $version for $os/$arch"
     TARGET="eiblog-$version.$os-$arch"
     GOOS=$os GOARCH=$arch CGO_ENABLED=0 go build
-    tar czvf $TARGET.tar.gz conf static views eiblog
-    rm eiblog
+    if [ "$os" == "windows" ]; then
+        tar czvf $TARGET.tar.gz conf static views eiblog.exe
+        rm eiblog.exe
+    else
+        tar czvf $TARGET.tar.gz conf static views eiblog
+        rm eiblog
+    fi
 done
