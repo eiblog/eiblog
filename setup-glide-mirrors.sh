aea028329064dc4fd3ad8a1f9558197e6f2f23be
weiwei.xiao <610961191@qq.com>
2018-08-09 09:50:19 +0800

    add glide mirrors

---
+++ setup-glide-mirrors.sh
@@ -0,0 +1,8 @@
+glide mirror set https://golang.org/x/crypto https://github.com/golang/crypto --vcs git
+glide mirror set https://golang.org/x/image https://github.com/golang/image --vcs git
+glide mirror set https://golang.org/x/mobile https://github.com/golang/mobile --vcs git
+glide mirror set https://golang.org/x/net https://github.com/golang/net --vcs git
+glide mirror set https://golang.org/x/net/context https://github.com/golang/net/context --vcs git
+glide mirror set https://golang.org/x/sys https://github.com/golang/sys --vcs git
+glide mirror set https://golang.org/x/text https://github.com/golang/text --vcs git
+glide mirror set https://golang.org/x/tools https://github.com/golang/tools --vcs git
