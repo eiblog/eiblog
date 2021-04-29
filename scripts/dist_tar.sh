c06f02622abf34e666a64252baecc8cfa430ba19
deepzz0 <deepzz.qi@gmail.com>
2021-04-29 12:31:30 +0800

    fix: tar file

--- scripts/dist_tar.sh
+++ scripts/dist_tar.sh
@@ -5,23 +5,17 @@ set -e
 _tag="$1"
 _arch=$(go env GOARCH)
 
-# prepare dir ./bin
-mkdir -p ./bin
-
 for file in pkg/core/*; do
   app="$(basename $file)";
   # tar platform
   for os in linux darwin windows; do
     _target="$app-$_tag.$os-$_arch.tar.gz"
     CGO_ENABLED=0 GOOS=$os GOARCH=$_arch \
-      go build -o bin/backend "./cmd/$app"
+      go build -o backend "./cmd/$app"
     if [ "$app" = "eiblog" ]; then
-      tar czf $_target conf website assets bin/backend
+      tar czf $_target conf website assets backend
     else
-      tar czf $_target conf bin/backend
+      tar czf $_target conf backend
     fi
   done
 done
-
-# clean dir ./bin
-rm -rf ./bin
