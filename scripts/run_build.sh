82fba0ddb47c1f66cb659f775c120c08dd2b4447
henry.chen <henry.chen@trustasia.com>
2021-10-26 16:17:50 +0800

    fix(release): github action runner

--- scripts/run_build.sh
+++ scripts/run_build.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env sh
 
-set -e
-
 _registry="$1"
 _tag="$2"
 _platform="linux/amd64,linux/arm64,linux/386"
@@ -11,11 +9,14 @@ if [ -z "$_registry" ] || [ -z "$_tag" ]; then
   exit 0;
 fi
 
+# create and use builder
+docker buildx inspect builder >/dev/null 2>&1
+if [ "$?" != "0" ]; then
+  docker buildx create --use --name builder
+fi
+
 # prepare dir ./bin
 mkdir -p ./bin
-# create builder
-docker buildx create --use --name builder
-
 # build demo app
 for file in pkg/core/*; do
   app="$(basename $file)";
