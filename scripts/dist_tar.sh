3b2a6689be07b234f0984aad51e51399f480366c
henry.chen <henry.chen@trustasia.com>
2022-10-01 21:18:53 +0800

    chore: update

--- scripts/dist_tar.sh
+++ scripts/dist_tar.sh
@@ -10,8 +10,8 @@ for file in pkg/core/*; do
   # tar platform
   for os in linux darwin windows; do
     _target="$app-$_tag.$os-$_arch.tar.gz"
-    CGO_ENABLED=0 GOOS=$os GOARCH=$_arch \
-      go build -tags prod -o backend "./cmd/$app"
+    GOOS=$os GOARCH=$_arch \
+      go build -tags prod -ldflags '-extldflags "-static"' -o backend "./cmd/$app"
     if [ "$app" = "eiblog" ]; then
       tar czf $_target conf website assets backend
     else
