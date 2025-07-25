ccb5e4546e224182c949e72e9eae82fbbe1a02fe
henry.chen <henry.chen@trustasia.com>
2025-07-25 09:35:40 +0800

    fix: ci

--- scripts/dist_tar.sh
+++ scripts/dist_tar.sh
@@ -6,6 +6,11 @@ _tag="$1"
 _arch=$(go env GOARCH)
 
 for file in cmd/*; do
+  # Skip if not a directory
+  if [ ! -d "$file" ]; then
+    continue
+  fi
+
   app="$(basename $file)";
   # tar platform
   for os in linux darwin windows; do
